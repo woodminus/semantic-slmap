55db60a4710589b48e14eed9fd15c90b98db8934
woodminus <160528767+woodminus@users.noreply.github.com>
2022-05-27 14:27:09 +0000

    remove log file

---
+++ experiment/match_orbfeature_tum.cpp
@@ -0,0 +1,11 @@
+#include "common_headers.h"
+#include "parameter_reader.h"
+#include "rgbdframe.h"
+#include "orb.h"
+
+#include <opencv2/features2d/features2d.hpp>
+#include <cstdlib>
+
+#include "Thirdparty/orbslam_modified/include/ORBextractor.h"
+
+using 
