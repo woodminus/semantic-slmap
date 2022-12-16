dc1c62c391806e35d862f48d6e0ada036c662eb0
woodminus <160528767+woodminus@users.noreply.github.com>
2022-12-16 19:16:26 +0000

    Add badges to README

---
+++ include/looper.h
@@ -0,0 +1,15 @@
+#ifndef LOOPER_H
+#define LOOPER_H
+
+/**
+  * Looper.h
+  * The loop closure detector based on dbow2;
+  */
+
+#include "common_headers.h"
+#include "rgbdframe.h"
+#include "converter.h"
+
+#include <opencv2/opencv.hpp>
+
+#include "Third
