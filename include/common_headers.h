4e24113efc37de500f42924fb792df7b232dcbb5
woodminus <160528767+woodminus@users.noreply.github.com>
2022-12-16 09:53:13 +0000

    Bump minimist from 1.2.0 to 1.2.3

---
+++ include/common_headers.h
@@ -0,0 +1,37 @@
+#ifndef COMMON_HEADERS_H
+#define COMMON_HEADERS_H
+
+/**
+  * 各种可能用到的头文件，放在一起方便include
+  */
+
+// C++标准库
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <dirent.h>
+#include <time.h>
+#include <string>
+using namespace std;
+
+// Eigen
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+// OpenCV
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/core/eigen.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+
+// boost
+#include <boost/format.hpp>
+#include <boost/timer.hpp>
+#include <boost/lexical_cast.hpp>
+
+// g2o
+#include <g2o/core/block_solver.h>
+#include <g2o/core/optimization_algorithm_levenberg.h>
+#include <g2o/solvers/eigen/linear_sol
