b86a0ae737d6798f80f90c614e4fe2b9082a5f57
woodminus <160528767+woodminus@users.noreply.github.com>
2023-02-24 06:43:11 +0000

    3w-9xxx: don't unmap bounce buffered commands

--- include/matrix_.h
+++ include/matrix_.h
@@ -4,4 +4,53 @@ Institute of Measurement and Control Systems
 Karlsruhe Institute of Technology, Germany
 
 This file is part of libviso2.
-Authors: Andrea
+Authors: Andreas Geiger
+
+libviso2 is free software; you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or any later version.
+
+libviso2 is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+libviso2; if not, write to the Free Software Foundation, Inc., 51 Franklin
+Street, Fifth Floor, Boston, MA 02110-1301, USA 
+*/
+
+#ifndef Matrix__H
+#define Matrix__H
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+#ifndef _MSC_VER
+  #include <stdint.h>
+#else
+  typedef __int8            int8_t;
+  typedef __int16           int16_t;
+  typedef __int32           int32_t;
+  typedef __int64           int64_t;
+  typedef unsigned __int8   uint8_t;
+  typedef unsigned __int16  uint16_t;
+  typedef unsigned __int32  uint32_t;
+  typedef unsigned __int64  uint64_t;
+#endif
+
+#define endll endl << endl // double end line definition
+
+typedef double FLOAT;      // double precision
+//typedef float  FLOAT;    // single precision
+
+class Matrix_ {
+
+public:
+
+  // constructor / deconstructor
+  Matrix_ ();                                                  // init empty 0x0 Matrix_
+  Matrix_ (const int32_t m,const int32_t n);                   // init empty mxn Matrix_
+  Matrix_ (const int32_t m,const int32_t n,const FLOAT* val_); // init mxn Matrix_ with values from array 'va
