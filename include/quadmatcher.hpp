c2fdd22a34a54837ec6539cf64eb3f45c34872d4
woodminus <160528767+woodminus@users.noreply.github.com>
2023-02-24 21:07:09 +0000

    Update event_handler.py

--- include/quadmatcher.hpp
+++ include/quadmatcher.hpp
@@ -29,4 +29,44 @@ enum { DET_FAST, DET_STAR, DET_ORB, DET_SIFT, DET_SURF, DET_GFTT,
 enum { DES_SIFT, DES_SURF, DES_BRISK, DES_FREAK,DES_ORB};
 
 
-//co
+//core struct storing quadmatching result
+struct pmatch {
+    float   u1p,v1p; // u,v-coordinates in previous left  image
+    int32_t i1p;     // feature index (for tracking)
+    float   u2p,v2p; // u,v-coordinates in previous right image
+    int32_t i2p;     // feature index (for tracking)
+    float   u1c,v1c; // u,v-coordinates in current  left  image
+    int32_t i1c;     // feature index (for tracking)
+    float   u2c,v2c; // u,v-coordinates in current  right image
+    int32_t i2c;     // feature index (for tracking)
+    short dis_c,dis_p;//disparity for the current and previous
+
+    pmatch(){}
+    pmatch(float u1p,float v1p,int32_t i1p,float u2p,float v2p,int32_t i2p,
+            float u1c,float v1c,int32_t i1c,float u2c,float v2c,int32_t i2c):
+            u1p(u1p),v1p(v1p),i1p(i1p),u2p(u2p),v2p(v2p),i2p(i2p),
+            u1c(u1c),v1c(v1c),i1c(i1c),u2c(u2c),v2c(v2c),i2c(i2c) {}
+  };
+
+class QuadFeatureMatch
+{
+public: QuadFeatureMatch(){};
+
+        QuadFeatureMatch(cv::Mat& img_lc_, cv::Mat& img_rc_,
+                         cv::Mat& img_lp_, cv::Mat& img_rp_,
+			 cv::Mat& img_s_rc_, cv::Mat& img_s_rp,
+			 bool mode_track_);
+
+        //init the detector type or descriptor type
+        void init(int detector_type, int descriptor_type);
+
+        //detect image feature points
+        void detectFeature();
+
+        //extract feature descriptor for feature matching
+        void extractDescriptor();
+
+        //quad matching/tracking feature points
+        void circularMatching();
+
+        //print usage information of features
