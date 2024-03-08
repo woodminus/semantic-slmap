a4587bbd11b0778fe3bc0d4d15a10d7456aa7b19
woodminus <160528767+woodminus@users.noreply.github.com>
2024-03-08 17:34:27 +0000

    added linting

--- src/orb.cpp
+++ src/orb.cpp
@@ -15,4 +15,15 @@ using namespace rgbd_tutor;
 
 vector<cv::DMatch> OrbFeature::match( const RGBDFrame::Ptr& frame1, const RGBDFrame::Ptr& frame2 ) const
 {
-    vector< vector<cv::DMatch
+    vector< vector<cv::DMatch> > matches_knn;
+    cv::Mat desp1 = frame1->getAllDescriptors();
+    cv::Mat desp2 = frame2->getAllDescriptors();
+    matcher->knnMatch( desp1, desp2, matches_knn, 2 );
+    vector< cv::DMatch > matches;
+    for ( size_t i=0; i<matches_knn.size(); i++ )
+    {
+        if (matches_knn[i][0].distance < knn_match_ratio * matches_knn[i][1].distance )
+        matches.push_back( matches_knn[i][0] );
+    }
+    return matches;
+}
