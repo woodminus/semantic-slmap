22d57814801f589843cd5ef3421a3b8dab384036
woodminus <160528767+woodminus@users.noreply.github.com>
2024-06-08 11:01:53 +0000

    Changed footer

--- src/vo_stereo.cpp
+++ src/vo_stereo.cpp
@@ -44,4 +44,61 @@ bool VisualOdometryStereo::Process(QuadFeatureMatch& quadmatcher)
     return updateMotion();
 }
 
-vector<double> VisualOdometrySt
+vector<double> VisualOdometryStereo::estimateMotion (std::vector<pmatch> &quadmatches) {
+
+  // return value
+  bool success = true;
+
+  // compute minimum distance for RANSAC samples
+  double width=0,height=0;
+
+  for (vector<pmatch>::iterator it=quadmatches.begin(); it!=quadmatches.end(); it++) {
+    if (it->u1c>width)  width  = it->u1c;
+    if (it->v1c>height) height = it->v1c;
+  }
+
+  // get number of matches
+  int N  = quadmatches.size();
+  if (N<6)
+    return vector<double>();
+
+  // allocate dynamic memory
+  X          = new double[N];
+  Y          = new double[N];
+  Z          = new double[N];
+  J          = new double[4*N*6];
+  p_predict  = new double[4*N];
+  p_observe  = new double[4*N];
+  p_residual = new double[4*N];
+
+  // project matches of previous image into 3d
+  for (int i=0; i<N; i++) {
+    double d = max(quadmatches[i].u1p - quadmatches[i].u2p,1.0f);
+    X[i] = (quadmatches[i].u1p-param.calib.cu)*param.base/d;
+    Y[i] = (quadmatches[i].v1p-param.calib.cv)*param.base/d;
+    Z[i] = param.calib.f*param.base/d;
+  }
+
+  // loop variables
+  vector<double> tr_delta;
+  vector<double> tr_delta_curr;
+  tr_delta_curr.resize(6);
+
+  // clear parameter vector
+  inliers.clear();
+
+  // initial RANSAC estimate
+  for (int k=0;k<param.ransac_iters;k++) {
+
+    // draw random sample set
+    vector<int> active = getRandomSample(N,3);
+
+    // clear parameter vector
+    for (int i=0; i<6; i++)
+      tr_delta_curr[i] = 0;
+
+    // minimize reprojection errors
+    VisualOdometryStereo::result result = UPDATED;
+    int iter=0;
+    while (result==UPDATED) {
+      result = updateParameters(quadmatches,active,tr_delta_c
